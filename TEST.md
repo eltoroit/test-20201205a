cf948afe75694dd21aa18d649b1d9f4b45d4b020
Andres Perez <aperez@salesforce.com>
2020-12-05 12:17:43 -0500

    Project cleaned

--- TEST.md
+++ TEST.md
@@ -7,5 +7,5 @@ git push origin master
 
 ## Heroku
 
-git remote add heroku https://git.heroku.com/test-20201205a.git
+git remote add heroku https://git.heroku.com/test-20201205a-stage.git
 git push heroku master
--- src/modules/c/home/home.js
+++ src/modules/c/home/home.js
@@ -1,12 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class Home extends LightningElement {
-    onBtnClick() {
-        // let start = new Date();
-        // let stop = start.setSeconds(start.getSeconds() + 5);
-        // while (Date.now() < stop) {
-        //     let dttm = new Date();
-        //     console.log(dttm.toJSON() + ' ' + dttm.getMilliseconds());
-        // }
-    }
-}
+export default class Home extends LightningElement {}
